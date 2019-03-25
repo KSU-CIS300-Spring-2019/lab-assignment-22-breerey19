413af7a72065fb5d5d91616ff513a78379e5a62c
breerey19 <breerey19@ksu.edu>
2019-03-25 15:21:49 -0500

    incomplete lab 22, 2 errors

---
+++ Ksu.Cis300.TrieLibrary/ITrie.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.TrieLibrary
+{
+    /// <summary>
+    /// An interface for a trie.
+    /// </summary>
+    public interface ITrie
+    {
+        /// <summary>
+        /// Determines whether this trie contains the given string.
+        /// </summary>
+        /// <param name="s">The string to look for.</param>
+        /// <returns>Whether this trie contains s.</returns>
+        bool Contains(string s);
+
+        /// <summary>
+        /// Adds the given string to this trie.
+        /// </summary>
+        /// <param name="s">The string to add.</param>
+        /// <returns>The resulting trie.</returns>
+        ITrie Add(string s);
+
+    }
+
+        public class TrieWithNoChildren : ITrie
+        {
+        private bool _IsEmpty = false;
+            public ITrie Add(string s)
+            {
+
+            if (s == "")
+            {
+                _IsEmpty = true;
+                return this;
+            }
+           
+            else
+            {
+                return new TrieWithOneChild(s, _IsEmpty);
+            }
+        }
+
+            public bool Contains(string s)
+            {
+            if (s == "")
+            {
+                return _IsEmpty;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        }
+    
+}
---
+++ Ksu.Cis300.TrieLibrary/TrieWithOneChild.cs
@@ -0,0 +1,89 @@
+﻿/*TrieWithOneChild.cs
+ * Bree Reynoso
+ */ 
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.TrieLibrary
+{
+    public class TrieWithOneChild : ITrie
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        private bool _IsEmpty;
+        private ITrie _child;
+        private char _label;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="s"></param>
+        public TrieWithOneChild(string s, bool b)
+        {
+            if(s == "" || (_label > 'a' && _label < 'z'))
+            {
+                throw new ArgumentException();
+            }
+            _IsEmpty = b;
+            _label = s[0];
+
+            ITrie t = new TrieWithNoChildren();
+            _child = t.Add(s.Substring(1));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public ITrie Add(string s)
+        {
+            if (s == "")
+            {
+                _IsEmpty = true;
+                return this;
+            }
+            else if (s[0] < 'a' || s[0] > 'z')
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                ITrie t = new TrieWithManyChildren(s, _IsEmpty,_label,_child);
+                return t;
+            }
+            
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public bool Contains(string s)
+        {
+            if (s == "")
+            {
+                return _IsEmpty;
+            }
+            else if (s[0] < 'a' || s[0] > 'z')
+            {
+                throw new ArgumentException();
+            }
+           
+            else
+            {
+                return _child.Contains(s.Substring(1));
+            }
+
+
+        }
+    }
+}
